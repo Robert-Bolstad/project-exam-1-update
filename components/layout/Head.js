50abd17091f108a9fa97133567af78e946b0c30b
Robert Bolstad <robert.s.bolstad@gmail.com>
2021-06-06 20:36:18 +0200

    logo added

--- components/layout/Head.js
+++ components/layout/Head.js
@@ -8,7 +8,7 @@ export default function Head({ title = "", description = "" }) {
         {title ? " | " : ""}Launch Prep
       </title>
       <meta name="description" content={description}></meta>
-      <link rel="icon" href="/favicon.svg" />
+      <link rel="icon" href="/logo.svg" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans&amp;family=Space+Mono&amp;display=swap"
